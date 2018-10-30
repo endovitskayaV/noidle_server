59701ccf8947b96fa6884b070dce3745bac1ee68
Valentina Endovitskaya <endovitskaya@reksoft.ru>
2018-10-30 08:13:46 +0300

    changed flyway config

--- src/main/java/ru/vsu/noidle_server/Migrations.java
+++ src/main/java/ru/vsu/noidle_server/Migrations.java
@@ -7,7 +7,7 @@ import org.flywaydb.core.Flyway;
 @SpringBootApplication
 public class Migrations {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  throws Exception{
         Flyway flyway = new Flyway();
         flyway.setDataSource(System.getenv("JDBC_DATABASE_URL"),
                 System.getenv("JDBC_DATABASE_USERNAME"),
