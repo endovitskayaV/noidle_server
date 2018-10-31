6a06683f6ff8fa85c02ee043111d88a1d5bf77ae
Valentina Endovitskaya <valyalark@hotmail.com>
2018-10-31 21:34:21 +0300

    add sign in with github

--- src/main/java/ru/vsu/noidle_server/controller/UserController.java
+++ src/main/java/ru/vsu/noidle_server/controller/UserController.java
@@ -1,41 +1,15 @@
 package ru.vsu.noidle_server.controller;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
-import ru.vsu.noidle_server.model.UserEntity;
-import ru.vsu.noidle_server.service.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.UUID;
+import java.security.Principal;
 
 @RestController
-@RequiredArgsConstructor
-@RequestMapping("/users")
 public class UserController {
-    private final UserService dataService;
 
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<UserEntity> getDataById(@PathVariable UUID id) {
-        UserEntity data = dataService.getById(id);
-        if (data != null) {
-            return ResponseEntity.ok(data);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
-
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<UserEntity>> getAllData() {
-        List<UserEntity> data = dataService.getAll();
-        return ResponseEntity.ok(data);
-    }
-
-    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<UserEntity> saveData(@RequestBody UserEntity dataEntity) {
-        return ResponseEntity.ok(dataService.saveDataEntity(dataEntity));
+    @RequestMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;
     }
 }
