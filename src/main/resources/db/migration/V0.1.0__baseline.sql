1bc0fbbf06d3a71a34614bb588f147b7f29a2dd3
Valentina Endovitskaya <endovitskaya@reksoft.ru>
2018-11-15 09:09:04 +0300

    refactoring

--- src/main/resources/db/migration/V0.1.0__baseline.sql
+++ src/main/resources/db/migration/V0.1.0__baseline.sql
@@ -1,9 +1,9 @@
 create table user_data (
-  id uuid primary key,
+  id    uuid primary key,
   email text unique not null,
-  name text  not null,
+  name  text        not null,
   photo text
 );
 
-insert  into user_data (id, email, name, photo) values
-('123e4567-e89b-12d3-a456-556642440000', 'email', 'test_user', 'photo')
+insert into user_data (id, email, name, photo)
+values ('123e4567-e89b-12d3-a456-556642440000', 'email', 'test_user', 'photo')
--- src/main/resources/db/migration/V0.1.2__level.sql
+++ src/main/resources/db/migration/V0.1.2__level.sql
@@ -1,43 +1,42 @@
-create table level(
+create table level (
   "order" bigint not null primary key,
-  name text not null
+  name    text   not null
 );
 
-insert into level ("order", name) values
-  (0, 'Somelevel'),
-  (1, 'Junior'),
-  (2, 'Somelevel'),
-  (3, 'Somelevel'),
-  (4, 'Somelevel'),
-  (5, 'Somelevel'),
-  (6, 'Somelevel'),
-  (7, 'Somelevel');
+insert into level ("order", name)
+values (0, 'Somelevel'),
+       (1, 'Junior'),
+       (2, 'Somelevel'),
+       (3, 'Somelevel'),
+       (4, 'Somelevel'),
+       (5, 'Somelevel'),
+       (6, 'Somelevel'),
+       (7, 'Somelevel');
 
 
-alter table user_data add column level_order bigint not null default 0 references level("order");
+alter table user_data
+  add column level_order bigint not null default 0 references level ("order");
 
 create table requirement (
-  id bigint primary key,
-  type text not null,
-  sub_type text not null,
-  level_order bigint not null references level("order"),
-  name text,
-  value bigint not null,
+  id          bigint primary key,
+  type        text   not null,
+  sub_type    text   not null,
+  level_order bigint not null references level ("order"),
+  name        text,
+  value       bigint not null,
   unique (type, sub_type, name)
 );
 
-insert into requirement (id, type, sub_type, level_order, value) values
-  (1, 'time','per_life', 1, 0),
-  (2, 'time','per_life', 2, 100),
-  (3, 'time','per_life', 3, 200),
-
-  (4, 'symbol','per_life', 1, 10),
-  (5, 'symbol','per_life', 2, 1000),
-  (6, 'symbol','per_life', 3, 2000),
-
-  (7, 'time','continuous_per_life', 1, 0),
-  (8, 'time','continuous_per_life', 2, 1000*60*60),
-  (9, 'time','continuous_per_life', 3, 1000*60*60*2);
+insert into requirement (id, type, sub_type, level_order, value)
+values (1, 'time', 'per_life', 1, 0),
+       (2, 'time', 'per_life', 2, 100),
+       (3, 'time', 'per_life', 3, 200),
+       (4, 'symbol', 'per_life', 1, 10),
+       (5, 'symbol', 'per_life', 2, 1000),
+       (6, 'symbol', 'per_life', 3, 2000),
+       (7, 'time', 'continuous_per_life', 1, 0),
+       (8, 'time', 'continuous_per_life', 2, 1000 * 60 * 60),
+       (9, 'time', 'continuous_per_life', 3, 1000 * 60 * 60 * 2);
 
 --   (10, 'symbol','continuous_per_life', 1, 1),
 --   (11 ,'symbol','continuous_per_life', 2, 500),
