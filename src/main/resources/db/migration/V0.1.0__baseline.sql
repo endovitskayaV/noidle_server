de3336751e50493c48d7a94b328dcb33af5e5808
Valentina Endovitskaya <endovitskaya@reksoft.ru>
2018-11-27 08:26:43 +0300

    fixed team table, add test data

--- src/main/resources/db/migration/V0.1.0__baseline.sql
+++ src/main/resources/db/migration/V0.1.0__baseline.sql
@@ -5,8 +5,10 @@ create table user_data (
   photo text
 );
 
-insert into user_data (id, email, name, photo)
-values ('123e4567-e89b-12d3-a456-556642440000', 'email', 'test_user', 'photo');
+--test data
 
 insert into user_data (id, email, name, photo)
-values ('123e4567-e89b-12d3-a456-556642440001', 'email1', 'test_user', 'photo')
+  values ('123e4567-e89b-12d3-a456-556642440000', 'email', 'test_user', 'photo');
+
+insert into user_data (id, email, name, photo)
+  values ('123e4567-e89b-12d3-a456-556642440001', 'email1', 'test_user', 'photo')
--- src/main/resources/db/migration/V0.1.3__team.sql
+++ src/main/resources/db/migration/V0.1.3__team.sql
@@ -1,6 +1,6 @@
 create table team (
   id uuid primary key,
-  name text unique not null default concat('New team', (select row_number() from team)),
+  name text unique not null,
   photo text
 );
 
@@ -9,3 +9,12 @@ create table team (
   team_id uuid references team (id),
   constraint user_data_team_pk primary key (user_id, team_id)
 );
+
+--test data
+
+insert into team values
+   ('123e4567-e89b-12d3-a456-556642440000', 'new team', null);
+
+insert into user_data_team values
+  ('123e4567-e89b-12d3-a456-556642440000','123e4567-e89b-12d3-a456-556642440000'),
+  ('123e4567-e89b-12d3-a456-556642440001','123e4567-e89b-12d3-a456-556642440000');
