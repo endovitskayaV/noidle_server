bbfc11e3291c142edf402af72b9216e27a86bef2
Valentina Endovitskaya <endovitskaya@reksoft.ru>
2018-12-18 13:48:16 +0300

    changed test data

--- src/main/resources/db/migration/V0.1.0__baseline.sql
+++ src/main/resources/db/migration/V0.1.0__baseline.sql
@@ -11,4 +11,4 @@ insert into user_data (id, email, name, photo)
   values ('123e4567-e89b-12d3-a456-556642440000', 'email', 'test_user', 'photo');
 
 insert into user_data (id, email, name, photo)
-  values ('123e4567-e89b-12d3-a456-556642440001', 'email1', 'test_user', 'photo')
+  values ('123e4567-e89b-12d3-a456-556642440001', 'email1', 'test_user2', 'photo')
