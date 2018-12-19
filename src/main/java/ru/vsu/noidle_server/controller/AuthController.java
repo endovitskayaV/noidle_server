154f2c5a12bc641c6ae7956ac180613ab34a9007
Valentina Endovitskaya <endovitskaya@reksoft.ru>
2018-12-19 16:57:24 +0300

    feature/52 add logout

--- src/main/java/ru/vsu/noidle_server/controller/AuthController.java
+++ src/main/java/ru/vsu/noidle_server/controller/AuthController.java
@@ -1,9 +1,13 @@
 package ru.vsu.noidle_server.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.view.RedirectView;
 import ru.vsu.noidle_server.service.UserService;
 
@@ -20,7 +24,7 @@ public class AuthController {
     @GetMapping("/oauth")
     public RedirectView user(OAuth2Authentication user) {
         userService.save(user);
-        return new RedirectView("/");
+        return new RedirectView("/dashboard");
     }
 
 }
