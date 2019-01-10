37cc416c72344f4d01df310611a5e946dfca03fa
Valentina Endovitskaya <endovitskaya@reksoft.ru>
2019-01-10 11:49:16 +0300

    added tomcat dependency cannot deploy to tomcat

--- src/main/java/ru/vsu/noidle_server/NoidleServerApplication.java
+++ src/main/java/ru/vsu/noidle_server/NoidleServerApplication.java
@@ -2,9 +2,16 @@ package ru.vsu.noidle_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NoidleServerApplication {
+public class NoidleServerApplication  extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(NoidleServerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NoidleServerApplication.class, args);
