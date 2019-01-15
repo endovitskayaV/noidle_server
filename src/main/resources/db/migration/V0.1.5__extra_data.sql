1f0397ba65455e5f4e39dd077db37ee9d5f9f188
Valentina Endovitskaya <valyalark@hotmail.com>
2019-01-15 22:48:04 +0300

    added extra data

--- src/main/resources/db/migration/V0.1.5__extra_data.sql
+++ src/main/resources/db/migration/V0.1.5__extra_data.sql
@@ -1,14 +1,81 @@
 insert into requirement (id, type, sub_type, achievement_id, extra_value, value)
-values  (1000, 'time', 'per_life', 65, null, 1000 * 60 * 60 * 60 * 24 * 17),
-        (1001, 'time', 'per_life', 67, null, 1000 * 60 * 60 * 60 * 24 * 30 * 5),
+values  (1000, 'time', 'per_life', 35, null, 1000 * 60 * 60 * 60 * 24 * 17),
+        (1001, 'time', 'per_life', 36, null, 1000 * 60 * 60 * 60 * 24 * 30 * 5),
 
-        (1002, 'symbol', 'per_life', 66, null, 100000),
-        (1003, 'symbol', 'per_life', 68, null, 1000000),
+        (1002, 'symbol', 'per_life', 37, null, 100000),
+        (1003, 'symbol', 'per_life', 38, null, 1000000),
 
-        (1004, 'commit', 'per_life', 35, 'failed', 1),
-        (1005, 'commit', 'per_life', 36, 'failed', 20),
-        (1005, 'commit', 'per_life', 37, 'failed', 100),
+        (1004, 'commit', 'per_life', 39, 'failed', 1),
+        (1006, 'commit', 'per_life', 40, 'failed', 20),
+        (1007, 'commit', 'per_life', 41, 'failed', 100),
 
-        (1004, 'commit', 'per_life', 38, 'successful', 1),
-        (1005, 'commit', 'per_life', 36, 'failed', 20),
-        (1005, 'commit', 'per_life', 37, 'failed', 100),
+        (1008, 'commit', 'per_life', 42, 'successful', 1),
+        (1009, 'commit', 'per_life', 43, 'successful', 200),
+        (1010, 'commit', 'per_life', 44, 'successful', 1000),
+        (1011, 'commit', 'per_life', 45, 'successful', 5000),
+        (1012, 'commit', 'per_life', 46, 'successful', 10000),
+        (1013, 'commit', 'per_life', 47, 'successful', 50000),
+        (1014, 'commit', 'per_life', 48, 'successful', 100000),
+        (1015, 'commit', 'per_life', 49, 'successful', 500000),
+
+        (1016, 'time', 'per_day', 50, null, 1000 * 60 * 60 * 60 * 5),
+        (1017, 'time', 'per_day', 51, null, 1000 * 60 * 60 * 60 * 8),
+        (1018, 'time', 'per_day', 52, null, 1000 * 60 * 60 * 60 * 12),
+        (1019, 'time', 'per_day', 53, null, 1000 * 60 * 60 * 60 * 16),
+        (1020, 'time', 'per_day', 54, null, 1000 * 60 * 60 * 60 * 20),
+        (1021, 'time', 'per_day', 55, null, 1000 * 60 * 60 * 60 * 22),
+        (1022, 'time', 'per_day', 56, null, 1000 * 60 * 60 * 60 * 24),
+
+        (1023, 'single_key', 'per_life', 57, 'Ctrl', 2000),
+        (1024, 'single_key', 'per_life', 57, 'C', 1000),
+        (1025, 'single_key', 'per_life', 57, 'V', 1000),
+
+        (1026, 'single_key', 'per_life', 58, 'Ctrl', 6000),
+        (1027, 'single_key', 'per_life', 58, 'C', 3000),
+        (1028, 'single_key', 'per_life', 58, 'V', 3000),
+
+        (1029, 'single_key', 'per_life', 59, 'Ctrl', 20000),
+        (1030, 'single_key', 'per_life', 59, 'C', 10000),
+        (1031, 'single_key', 'per_life', 59, 'V', 10000),
+
+        (1029, 'single_key', 'per_life', 60, 'Ctrl', 100000),
+        (1030, 'single_key', 'per_life', 60, 'C', 50000),
+        (1031, 'single_key', 'per_life', 60, 'V', 50000),
+
+        (1032, 'single_key', 'per_life', 61, 'F5', 1000),
+        (1033, 'single_key', 'per_life', 61, 'F8', 1000),
+        (1033, 'single_key', 'per_life', 61, 'F9', 1000),
+        (1033, 'single_key', 'per_life', 61, 'F10', 1000),
+
+        (1034, 'single_key', 'per_life', 62, 'F5', 5000),
+        (1035, 'single_key', 'per_life', 62, 'F8', 5000),
+        (1036, 'single_key', 'per_life', 62, 'F9', 5000),
+        (1037, 'single_key', 'per_life', 62, 'F10', 5000),
+
+        (1038, 'single_key', 'per_life', 63, 'F5', 10000),
+        (1039, 'single_key', 'per_life', 63, 'F8', 10000),
+        (1040, 'single_key', 'per_life', 63, 'F9', 10000),
+        (1041, 'single_key', 'per_life', 63, 'F10', 10000),
+
+        (1042, 'single_key', 'per_life', 64, 'F5', 50000),
+        (1043, 'single_key', 'per_life', 64, 'F8', 50000),
+        (1044, 'single_key', 'per_life', 64, 'F9', 50000),
+        (1045, 'single_key', 'per_life', 64, 'F10', 50000),
+
+        (1046, 'single_key', 'per_life', 65, 'Space', 15000),
+        (1047, 'single_key', 'per_life', 66, 'Tab', 15000),
+
+        (1048, 'exec', 'per_life', 67, 'failed', 1),
+        (1049, 'exec', 'per_life', 68, 'failed', 100),
+        (1050, 'exec', 'per_life', 69, 'failed', 1000),
+        (1051, 'exec', 'per_life', 70, 'failed', 10000),
+        (1052, 'exec', 'per_life', 71, 'failed', 50000);
+
+
+        (1053, 'time', 'per_life', 72, null, 1000 * 60 * 60 * 60 * 24 * 365);
+        (1054, 'symbols', 'per_life', 73, null, 1000000);
+        (1055, 'commit', 'per_life', 74, 'successful', 500);
+        (1056, 'exec', 'per_life', 75, 'successful', 1000);
+        (1057, 'commit', 'per_life', 76, 'failed', 500);
+        (1058, 'exec', 'per_life', 77, 'failed', 1000);
+        (1059, 'single_key', 'per_life', 78, 'Ctrl', 1000);
