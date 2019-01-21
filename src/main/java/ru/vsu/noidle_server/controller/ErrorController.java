e6097b543f38021268881990ea22ca831df9e89c
Valentina <evvEndovitskaya@yandex.ru>
2019-01-21 12:35:38 +0300

    hotfix/111 fixed link to resources (#10)

---
+++ src/main/java/ru/vsu/noidle_server/controller/ErrorController.java
@@ -0,0 +1,25 @@
+package ru.vsu.noidle_server.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import ru.vsu.noidle_server.Constants;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+
+    @GetMapping(Constants.ERROR_URI)
+    public ModelAndView getErrorPage(ModelMap modelMap, HttpServletRequest httpServletRequest) {
+        modelMap.addAttribute("url", httpServletRequest.getRequestURL().toString()
+                .replace(httpServletRequest.getRequestURI(), ""));
+        return new ModelAndView("error", modelMap);
+    }
+
+    @Override
+    public String getErrorPath() {
+        return Constants.ERROR_URI;
+    }
+}
